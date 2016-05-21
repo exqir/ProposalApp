9cf7b30f9499dbfb825fbab30c934b871a5fb5ad
exqir <maltesellmer@Maltes-Air.fritz.box>
2016-05-21 18:51:46 +0200

    switched to scss, added gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,31 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
-gulp.task('default', function() {
+var input = './app/scss/*.scss';
+var output = './app/css';
+
+gulp.task('sass', function () {
+  return gulp
+    // Find all `.scss` files from the `stylesheets/` folder
+    .src(input)
+    // Run Sass on those files
+    .pipe(sass())
+    // Write the resulting CSS in the output folder
+    .pipe(gulp.dest(output));
+});
+
+gulp.task('watch', function() {
+  return gulp
+    // Watch the input folder for change,
+    // and run `sass` task when something happens
+    .watch(input, ['sass'])
+    // When there is a change,
+    // log a message in the console
+    .on('change', function(event) {
+      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+});
+
+gulp.task('default', ['sass', 'watch'], function() {
   // place code for your default task here
 });
