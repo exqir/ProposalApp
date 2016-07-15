4f04830f6f971185f76e2f0ed6dc286c310c9758
exqir <maltesellmer@Maltes-MacBook-Air.local>
2016-07-15 11:35:28 +0200

    added gulp build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
+var buildFolder = './build/';
 var input = './app/scss/*.scss';
-var output = './app/css';
+var output = './build/app/css';
 
 gulp.task('sass', function () {
   return gulp
@@ -25,6 +26,54 @@ gulp.task('watch', function() {
       console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
     });
 });
+
+gulp.task('build',['copy-home','copy-rest','copy-php','copy-app','sass'], function () {
+    console.log('building project...')
+})
+
+gulp.task('copy-home', function() {
+   return gulp
+       .src('./*.{php,php70}')
+       .pipe(gulp.dest(buildFolder));
+});
+
+gulp.task('copy-rest', function () {
+    return gulp
+        .src('./restEndpoint/index.php')
+        .pipe(gulp.dest(buildFolder + 'restEndpoint'));
+});
+
+gulp.task('copy-php', function () {
+    return gulp
+        .src('./classes/*.php')
+        .pipe(gulp.dest(buildFolder + 'classes'));
+})
+
+gulp.task('copy-js', function () {
+    return gulp
+        .src('./app/js/*.{js,map}')
+        .pipe(gulp.dest(buildFolder + 'app/js'));
+})
+
+gulp.task('copy-partials', function () {
+    return gulp
+        .src('./app/partials/*.html')
+        .pipe(gulp.dest(buildFolder + 'app/partials'));
+})
+
+gulp.task('copy-assets', function () {
+    return gulp
+        .src('./app/assets/*.{jpeg,png,jpg}')
+        .pipe(gulp.dest(buildFolder + 'app/assets'));
+})
+
+gulp.task('copy-app',['copy-js','copy-assets','copy-partials'], function () {
+    return gulp
+        .src('./app/*.html')
+        .pipe(gulp.dest(buildFolder + 'app'));
+})
+
+
 
 gulp.task('default', ['sass', 'watch'], function() {
   // place code for your default task here
